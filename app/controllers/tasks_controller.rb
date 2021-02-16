0cb2ea367622a1782a5c733fdddb050ea3c074bb
cosmina iosifescu <cosmina.iosifescu@gmail.com>
2021-02-16 17:46:37 +0100

    Finished the task manager. Form can use some refactoring

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,11 +1,34 @@
 class TasksController < ApplicationController
-  before_action :find_task, only: [:show]
+  before_action :find_task, only: [:show, :edit, :update, :destroy]
 
   def index
     @tasks = Task.all
   end
 
   def show
+  end
+
+  def new
+    @task = Task.new
+  end
+
+  def create
+    @task = Task.new(task_params)
+    @task.save
+    redirect_to task_path(@task)
+  end
+
+  def edit
+  end
+
+  def update
+    @task.update(task_params)
+    redirect_to task_path(@task)
+  end
+
+  def destroy
+    @task.destroy
+    redirect_to tasks_path
   end
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,17 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  get 'tasks' , to: 'tasks#index'
-  get 'tasks/:id', to: 'tasks#show', as: :task
-  # get 'tasks/new', to: 'restaurants#new', as: :new_task
+  # # read
+  # get 'tasks' , to: 'tasks#index'
+  # # create
+  # get 'tasks/new', to: 'tasks#new', as: :new_task
+  # post 'tasks', to: 'tasks#create'
+  # # read one task
+  # get 'tasks/:id', to: 'tasks#show', as: :task
+  # # update task
+  # get '/tasks/:id/edit', to: 'tasks#edit', as: :edit_task
+  # patch '/tasks/:id', to: 'tasks#update'
+
+  # #delete task
+  # delete '/tasks/:id', to: 'tasks#destroy'
+  resources :tasks
 end
